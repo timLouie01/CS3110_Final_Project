4fc4a5038e6fb4fd442ebc36c725343f4726cc19
tjl96 <tjl96@cornell.edu>
2023-10-20 12:00:00 -0400

    Small change to shipsBag

--- lib/shipsBag.ml
+++ lib/shipsBag.ml
@@ -9,8 +9,11 @@ end
 
 module Player1List:PlayerListOfShips = struct
 
-  type t = Carrier.t | BattleShip.t | Destroyer.t | Submarine.t | PatrolBoat.t
+  (* type t = Carrier.t | BattleShip.t | Destroyer.t | Submarine.t | PatrolBoat.t *)
+  type t = {carrier :Carrier.t ; battleship: BattleShip.t ; destoryer : Destroyer.t ;
+   submarine : Submarine.t ; patrolBoat : PatrolBoat.t}
 
+  (* type t  = b list *)
   let remove_Carrier (input: Carrier.t) : bool 
   = failwith "unimplemented"
 
