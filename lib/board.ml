9c8ae3b75a0ef3d8fb07e2e65034cd960be581cd
tjl96 <tjl96@cornell.edu>
2023-10-22 23:11:49 -0400

    Updating board module and interface

--- lib/board.ml
+++ lib/board.ml
@@ -1,54 +1,61 @@
 open Ship
+open ShipsBag
+
 (* The signature of the GameBoard *)
 module type GameBoard = sig
+  type occupy =
+    | Ocean
+    | Carrier of AShip.t
+    | BattleShip of AShip.t
+    | Destroyer of AShip.t
+    | Submarine of AShip.t
+    | PatrolBoat of AShip.t
 
-  type 'a t
+  type t
 
-  type occupy = Ocean | Carrier of AShip.t | BattleShip of AShip.t | Destroyer of AShip.t
-  | Submarine of AShip.t | PatrolBoat of AShip.t
-
-  val ships_placed: occupy list
-  val n: int
-
-  val set_up_board: int -> 'a t
-
-  val valid_pos: int -> int -> bool
-  
-  val ship_positons : occupy -> int -> int -> int list
-
+  val set_up_board : t
+  val valid_pos : int -> int -> t -> bool
+  val ship_positons : occupy -> int -> int -> t -> int list
   val place_ship : occupy -> int -> int -> int -> int -> int
-
-  val search_ship: int -> int -> bool
-
-  val shoot: int -> int -> bool
-
-
+  val search_ship : int -> int -> bool
+  val shoot : int -> int -> bool
 end
 
-module BattleGround:GameBoard = struct
-  type 'a t = 'a array array
+module BattleGround : GameBoard = struct
+  type occupy =
+    | Ocean
+    | Carrier of AShip.t
+    | BattleShip of AShip.t
+    | Destroyer of AShip.t
+    | Submarine of AShip.t
+    | PatrolBoat of AShip.t
 
-  type occupy = Ocean | Carrier of AShip.t | BattleShip of AShip.t | Destroyer of AShip.t
-  | Submarine of AShip.t | PatrolBoat of AShip.t
-  let ships_placed = []
-  let n = 0
+  type t = {
+    board : occupy array array;
+    size : int;
+    fleet : Player1List.t;
+  }
 
-  let set_up_board (n' : int) :'a t = Array.make_matrix 10 10 occupy
+  let set_up_board : t =
+    {
+      board = Array.make_matrix 10 10 Ocean;
+      size = 10;
+      fleet = ShipsBag.build_List;
+    }
 
-  let valid_pos(x: int) (y: int): bool = if BattleGround.(x).(y) = Ocean then True else False
-  let ship_positons (ship:occupy) (x: int) (y: int): int list = 12
+  let valid_pos (x : int) (y : int) (b : t) : bool =
+    if b.board.(x).(y) = Ocean then true else false
 
-  let place_ship (ship:occupy) (x1: int) (y1: int) (x2: int) (y2: int): int = failwith "unimplemented"
+  let ship_positons (ship : occupy) (x : int) (y : int) (b : t) : int list =
+    b.board.(x).(y) = ship in 
 
-  let search_ship (x: int) (y: int): bool = failwith "unimplemented"
-  let shoot (x: int) (y: int): bool = failwith "unimplemented"
+
+  let place_ship (ship : occupy) (x1 : int) (y1 : int) (x2 : int) (y2 : int) :
+      int =
+    failwith "unimplemented"
+
+  let search_ship (x : int) (y : int) : bool = failwith "unimplemented"
+  let shoot (x : int) (y : int) : bool = failwith "unimplemented"
 end
 
-
-(* module BattleGround2:GameBoard = struct
-  include BattleGround1
-end *)
-
-
-
-  
+(* module BattleGround2:GameBoard = struct include BattleGround1 end *)
